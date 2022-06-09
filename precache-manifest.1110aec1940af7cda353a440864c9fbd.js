395b6bda95a103d5004fce62ecc488278f296e1f
mnkin <2664306866@qq.com>
2022-06-09 08:20:08 +0800

    deploy

---
+++ precache-manifest.1110aec1940af7cda353a440864c9fbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbfb8d6ff2d1acf2572c",
+    "url": "/test-morney-website/css/app.0aead38e.css"
+  },
+  {
+    "revision": "a8af5db104d9762ea0549aff094669f8",
+    "url": "/test-morney-website/index.html"
+  },
+  {
+    "revision": "bbfb8d6ff2d1acf2572c",
+    "url": "/test-morney-website/js/app.47ffd2af.js"
+  },
+  {
+    "revision": "ae8f2ede69e4f4e108b7",
+    "url": "/test-morney-website/js/chunk-vendors.2e390db1.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/test-morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-morney-website/robots.txt"
+  }
+]);
